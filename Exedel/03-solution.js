07fcb604f2e98d10b8aad35bd5856f4c5e7e6b72
Asliddin <asliddinshukurjonov@gmail.com>
2022-03-18 01:15:35 +0500

    minor changes

--- Exedel/03-solution.js
+++ Exedel/03-solution.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-// const inputArray = fs.readFileSync('input.txt', 'utf-8').split(',').map(number => Number(number));
+// const outputArray = fs.readFileSync('input.txt', 'utf-8').split(',').map(number => Number(number));
 
 let count = 0
-for (let i = 0; i < inputArray.length - 1; i++) {
-  if (inputArray[i] < inputArray[i + 1]) count++
+for (let i = 0; i < outputArray.length - 1; i++) {
+  if (outputArray[i] < outputArray[i + 1]) count++
 }
 
 fs.writeFileSync('output.txt', String(count));
